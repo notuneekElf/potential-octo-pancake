a7d66f4653ba81f11cc8e5e3bea7aca72c5c5647
parkingmechanic <53428480+notuneekElf@users.noreply.github.com>
2021-01-05 14:10:04 +0530

    Add files via upload

--- bitcoin.py
+++ bitcoin.py
@@ -1,7 +1,9 @@
 import pandas as pd;
 import seaborn as sns;
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 df=pd.read_csv(r"C:\Python39\Datasets\bitcoin_dataset.csv")
-df1=df.corr()
-sns.heatmap(df1)
+df1=df.fillna(df.mode())
+df2=df1.corr()
+sns.heatmap(df2)
 plt.show()
