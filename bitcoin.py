ea8cec81bf4cf31599569ffb4595102b00c7d45a
parkingmechanic <53428480+notuneekElf@users.noreply.github.com>
2021-01-05 22:29:36 +0530

    Add files via upload
    The task was to predict bitcoin prices on a particular day. First approach was to wing it by correlating date and market prices. Thank heavens I didn't stick with that approach.

--- bitcoin.py
+++ bitcoin.py
@@ -1,7 +1,18 @@
 import pandas as pd;
 import seaborn as sns;
 import matplotlib.pyplot as plt
+import datetime as dt
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error,r2_score
 df=pd.read_csv(r"C:\Python39\Datasets\bitcoin_dataset.csv")
-df1=df.corr()
-sns.heatmap(df1)
-plt.show()
+df1=df.fillna(df.mode())
+df1['Date']=pd.to_datetime(df1.Date)
+df1['Date']=df1['Date'].map(dt.datetime.toordinal)
+X=pd.DataFrame(df1['btc_market_price'])
+Y=pd.DataFrame(df1['btc_market_price'])
+reg=linear_model.LinearRegression()
+reg.fit(X,Y)
+Y_pred=reg.predict(X)
+rmse=mean_squared_error(Y,Y_pred)
+print(rmse)
