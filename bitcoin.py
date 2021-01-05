e2212b4c5660f1c77e63fd0eabc8f8329f4c7efb
parkingmechanic <53428480+notuneekElf@users.noreply.github.com>
2021-01-05 13:36:15 +0530

    Add files via upload
    The task was to find a perfect r value for btc_market_price. The answer isn't btc_market_price lolz

--- bitcoin.py
+++ bitcoin.py
@@ -2,6 +2,6 @@ import pandas as pd;
 import seaborn as sns;
 import matplotlib.pyplot as plt
 df=pd.read_csv(r"C:\Python39\Datasets\bitcoin_dataset.csv")
-df1=sns.load_dataset("df")
-sns.jointplot(x="x_corr",y="y_corr",data=df)
+df1=df.corr()
+sns.heatmap(df1)
 plt.show()
